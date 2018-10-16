0bb260555e31dfe5875a2de8da32922485e5c2c2
dev-pasa <dev5@uw.edu>
2018-10-15 22:31:46 -0700

    first script

--- js/app.js
+++ js/app.js
@@ -1,80 +1,55 @@
 'use strict';
 
-// Object literal notation
-
-// Let's model some Seattle beaches in code
-// Let's make note of the name, neighborhood, and seagull count of each beach for each day last week
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am' , '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-var 1stPike = {
+var firstAndPike = {
   name: '1st and Pike',
   neighborhood: 'Seattle',
-  seagullCount: [34, 77, 23, 347, 55, 72, 88]
-};
-
-
-alki.render = function() {
-  // access the element where we will attach lis
-  var alkiUl = document.getElementById('alkibeach');
-  // for each element in the seagullCount array, we need to
-  for (var i = 0; i < this.seagullCount.length; i++) {
-    // console.log(this.seagullCount[i], 'Alki gulls');
-    // 1. create a <li> element
-    var liEl = document.createElement('li');
-    // 2. give that <li> element content
-    liEl.textContent = `${days[i]}: ${this.seagullCount[i]}`;
-    // console.log(liEl, 'liEl')
-    // 3. append the <li> to the <ul>
-    alkiUl.appendChild(liEl);
-  }
+  minCust: Math.ceil(23),
+  maxCust: Math.floor(65),
+  avgCookieSale: 6.3,
+  totalCookies : 0,
+  
+  getCustomersPerHour: function(min, max) {
+    for (let i = 0; i < hours.length; i++) {
+      this.customersPerHour.push(Math.floor(Math.random() * (max - min + 1)) + min)
+    }
+  },
+  customersPerHour: [],
+  getCookiesPerHour: function() {
+    for (let i = 0; i < this.customersPerHour.length; i++) {
+      this.cookiesPerHour.push(Math.floor(this.customersPerHour[i] * this.avgCookieSale))
+    }
+  },
+  cookiesPerHour: [],
 }
 
-var goldenGardens = {
-  name: 'Golden Gardens Beach',
-  neighborhood: 'Ballard',
-  seagullCount: [12, 42, 33, 18, 2, 34, 17]
-};
+firstAndPike.getCustomersPerHour(firstAndPike.minCust, firstAndPike.maxCust)
+firstAndPike.getCookiesPerHour()
+console.log(firstAndPike);
 
-goldenGardens.render = function () {
-  // access the element where we will attach lis
-  var goldenGardensUl = document.getElementById('gg');
-  // for each element in the seagullCount array, we need to
-  for (var i = 0; i < this.seagullCount.length; i++) {
-    // console.log(this.seagullCount[i], 'goldenGardens gulls');
-    // 1. create a <li> element
-    var liEl = document.createElement('li');
-    // 2. give that <li> element content
-    liEl.textContent = `${days[i]}: ${this.seagullCount[i]}`;
-    // console.log(liEl, 'liEl')
-    // 3. append the <li> to the <ul>
-    goldenGardensUl.appendChild(liEl);
+firstAndPike.sumTotalCookie = function () {
+  for(var i = 0; i<this.cookiesPerHour.length; i++){
+    this.totalCookies+= this.cookiesPerHour[i]
   }
+  return this.totalCookies;
 }
 
-var edmonds = {
-  name: 'Edmonds Beach', 
-  neighborhood: 'Edmonds', 
-  seagullCount: [65, 48, 89, 1, 56, 62, 89]
-};
+firstAndPike.render = function() {
+  var pike = document.getElementById('firstAndPike');
 
+  for (var i = 0; i < this.cookiesPerHour.length; i++) {
 
-
-edmonds.render = function () {
-  // access the element where we will attach lis
-  var edmondsUl = document.getElementById('edmonds');  // for each element in the seagullCount array, we need to
-  for (var i = 0; i < this.seagullCount.length; i++) {
-    // console.log(this.seagullCount[i], 'edmonds gulls');
-    // 1. create a <li> element
     var liEl = document.createElement('li');
-    // 2. give that <li> element content
-    liEl.textContent = `${days[i]}: ${this.seagullCount[i]}`;
-    // console.log(liEl, 'liEl')
-    // 3. append the <li> to the <ul>
-    edmondsUl.appendChild(liEl);
+    liEl.textContent = `${hours[i]} : ${this.cookiesPerHour[i]}`;
+    pike.appendChild(liEl);
   }
+  liEl.textContent = `Total Cookies : ${this.totalCookies}`;
+  pike.appendChild(liEl);
+
 }
 
-alki.render();
-goldenGardens.render();
-edmonds.render();
+firstAndPike.sumTotalCookie();
+firstAndPike.render();
+
